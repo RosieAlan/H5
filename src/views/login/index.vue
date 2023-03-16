54b21a13ec32d4508caa339442f87f7018c1ca49
linjh <516308181@qq.com>
2023-03-16 09:09:51 +0800

    测试封装的工具函数request

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,38 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="login-page">
-    <cp-nav-bar title="登录" right-text="Rosie"></cp-nav-bar>
+  <div class="">
+    <van-button type="primary" @click="login">登录</van-button>
+    &nbsp; &nbsp;
+    <van-button type="primary" @click="getUserInfo">获取用户信息</van-button>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<script lang="ts" setup>
+import { request } from '@/utils/request'
+import { useUserStore } from '@/stores'
+import type { User } from '@/types/user'
+const store = useUserStore()
+// 登录
+const login = () => {
+  request<User>('login/password', 'POST', {
+    mobile: '13211112222',
+    password: 'abc12345'
+  })
+    .then(res => {
+      console.log(res)
+      store.setUser(res.data)
+    })
+    .catch(err => {
+      console.log('err is ', err)
+    })
+}
+// 获取用户信息
+const getUserInfo = () => {
+  request<User>('patient/myUser')
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(error => {
+      console.log('error is ', error)
+    })
+}
+</script>
