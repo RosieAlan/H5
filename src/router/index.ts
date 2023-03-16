ecbb6eb60e2d6b05b47644dce3eac890a653d3ea
linjh <516308181@qq.com>
2023-03-16 14:18:46 +0800

    页面标题

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,38 +5,49 @@ const router = createRouter({
   routes: [
     {
       path: '/login',
-      component: () => import('@/views/login/index.vue')
+      component: () => import('@/views/login/index.vue'),
+      meta: { title: '登录' }
     },
     {
       path: '/',
       component: () => import('@/views/layout/index.vue'),
       redirect: '/home',
       children: [
-        { path: '/home', 
-        component: () => import('@/views/home/index.vue') },
+        {
+          path: '/home',
+          component: () => import('@/views/home/index.vue'),
+          meta: { title: '首页' }
+        },
         {
           path: '/article',
-          component: () => import('@/views/article/index.vue')
+          component: () => import('@/views/article/index.vue'),
+          meta: { title: '健康百科' }
         },
         {
           path: '/notify',
-          component: () => import('@/views/notify/index.vue')
+          component: () => import('@/views/notify/index.vue'),
+          meta: { title: '消息通知' }
         },
-        { path: '/user', 
-        component: () => import('@/views/user/index.vue') }
+        {
+          path: '/user',
+          component: () => import('@/views/user/index.vue'),
+          meta: { title: '个人中心' }
+        }
       ]
     }
   ]
 })
 // 访问权限控制
-// 不需要登录的页面，白名单
-const wihteList = ['/login','/register']
-router.beforeEach(to => {
+router.beforeEach((to) => {
   // 用户仓库
   const store = useUserStore()
+  // 不需要登录的页面，白名单
+  const wihteList = ['/login']
   // 如果没有登录且不在白名单内，去登录
   if (!store.user?.token && !wihteList.includes(to.path)) return '/login'
   // 否则不做任何处理
 })
-
+router.afterEach((to) => {
+  document.title = `${to.meta.title}`
+})
 export default router
