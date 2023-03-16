2759d7f39d38a8dbe6966361ce70dce29f6c206f
linjh <516308181@qq.com>
2023-03-16 09:08:35 +0800

    抽取请求工具函数

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,60 +1,72 @@
-import axios from 'axios'
+import axios, { type Method } from 'axios'
 import { useUserStore } from '@/stores'
 import { showToast } from 'vant'
 import router from '@/router'
+import type { ApiRes } from '@/types/data'
 const baseURL = 'https://consult-api.itheima.net/'
 
-// 创建实例
+// 创建axios实例
 const instance = axios.create({
   baseURL,
-  timeout: 10000
+  timeout: 10000 // 超时时间10s
 })
 
-// 请求拦截器（携带token）
+// 请求拦截器
+// 会遇到问题:https://blog.csdn.net/m0_72976487/article/details/128559358
 instance.interceptors.request.use(
-  function (config) {
-    // 在发送请求之前做些什么
+  (config: any) => {
     const store = useUserStore()
     if (store.user?.token && config.headers) {
-      config.headers.Authorization = `Bearer ${store.user.token}`
+      config.headers['Authorization'] = `Bearer ${store.user?.token}`
     }
-    // 如果有则携带token
+
     return config
   },
-  function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error)
-  }
+  err => Promise.reject(err)
 )
 
-// 响应拦截器（处理token过期401）
+// 响应拦截器
 instance.interceptors.response.use(
-  function (res) {
-    // 2xx 范围内的状态码都会触发该函数。
+  res => {
     // 当http状态码是2xx的时候，会进入到该回调函数，但是http状态码ok，不代表业务也是ok的
     if (res.data.code !== 10000) {
       showToast(res.data.message || '网络异常')
       return Promise.reject(res.data)
     }
 
-    // 下面是正常返回响应体数据
+    // 只返回请求体的数据
     return res.data
   },
-  function (error) {
-    // 超出 2xx 范围的状态码都会触发该函数。
-    if (error.response.status === 401) {
-      // token过期了
-
-      // 清除token
+  err => {
+    // 当http的状态码是4xx、5xx的时候，会进入该回调函数
+    if (err.response.status === 401) {
+      // 我们这个项目，后台约定，token无效就是401
       const store = useUserStore()
-      store.delUser()
+      store.deleteUser()
 
-      // 跳转到登录页面，并且传递重定向页面的参数
-      // router.currentRoute.value.fullPath 当前路由的path
-      router.push(`/login?redirect=${router.currentRoute.value.fullPath}`)
+      // 跳转到登录页面，并且携带回调地址
+      router.push(`/login?returnUrl=${router.currentRoute.value.fullPath}`)
     }
-
-    return Promise.reject(error)
+    return Promise.reject(err)
   }
 )
-export { baseURL }
+
+// 封装请求工具函数，方便到时候，对应的api进行调用
+const request = <T>(
+  url: string,
+  method: Method = 'GET',
+  submitData?: object
+) => {
+  /**
+   * 参数1：泛型，但是这个类型，会赋值给res.data
+   * 参数2：服务器返回数据的泛型，这个数据直接会赋值给res
+   */
+  return instance.request<T, ApiRes<T>>({
+    url,
+    method,
+    [method.toLocaleLowerCase() === 'get' ? 'params' : 'data']: submitData
+  })
+}
+
+export { baseURL, request }
+
